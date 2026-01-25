430c4c2721ba44458e5cd70f01b766bca1d99887
Jaris Chow <jaris2203@gmail.com>
2026-01-25 23:44:21 +0800

    Add echo and asking of user name

--- src/main/java/TalkingPal.java
+++ src/main/java/TalkingPal.java
@@ -1,3 +1,4 @@
+import java.util.Objects;
 import java.util.Scanner;
 
 public class TalkingPal {
@@ -7,18 +8,34 @@ public class TalkingPal {
     public static void main(String[] args) {
 
         System.out.println(lineDivider + "Hello! I'm TalkingPal\n");
-        System.out.println("What can I do for you?\n" + lineDivider);
 
-        // Get user input
+        // Get user's name
         Scanner scanner = new Scanner(System.in);
+        System.out.println("What is your name?\n" + lineDivider);
+        String userName = scanner.nextLine();
+        System.out.println(lineDivider
+                + String.format("Greetings %s! What can I do for you?\n", userName)
+                + lineDivider);
+
+        // Get user input repeatedly until bye is said
         String userInput = scanner.nextLine();
+        while (!Objects.equals(userInput, "bye")) {
+            System.out.println(lineDivider
+                    + userInput + "\n"
+                    + lineDivider);
+            userInput = scanner.nextLine();
+        }
 
         // Greet and exit
         exitChat();
         scanner.close();
     }
 
+
     public static void exitChat() {
-        System.out.println(lineDivider + "Bye. Hope to see you again soon!\n");
+        System.out.println(lineDivider
+                + "Bye. Hope to see you again soon!\n"
+                + lineDivider);
     }
+
 }
